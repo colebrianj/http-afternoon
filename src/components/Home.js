46de86dd70a8e3a78a9fcbb995172d4ca9620dcf
colebrianj <cole.brianj@gmail.com>
2017-12-07 09:15:17 -0600

    message here

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Hero from './subcomponents/Hero';
 import BlogThumb from './subcomponents/BlogThumb';
-
-// import axios
+import axios from 'axios';
 
 class Home extends Component{
     constructor(){
@@ -13,14 +12,20 @@ class Home extends Component{
         }
     }
 
-    // insert componentWillMount:
+    componentWillMount(){
+        axios.get('/api/featured').then(results=>{
+            this.setState({
+                featured: results.data,
+                index: (~~(Math.random() * results.data.length) + 0),
+                posts: results.data
+            })
+        }).catch(console.log)   
+    }
+        render(){
+            // map over your recommended blogs here
+            const posts = this.state.posts.map((c,i)=><BlogThumb key={i} blog={c}/>)
     
-
-    render(){
-        // map over your recommended blogs here, replace null.
-        const posts = null
-
-        return(
+            return(
             <div className="content" >
                 <Hero blog={this.state.posts[this.state.index]} />
                 <hr/>
